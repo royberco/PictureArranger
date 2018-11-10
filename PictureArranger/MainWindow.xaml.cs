a0715db88b7468ec35e2ec69cb4073a541aa26e1
Roy Bercovich <roy.berco@gmail.com>
2018-11-10 19:13:44 +0200

    Start Point

--- PictureArranger/MainWindow.xaml.cs
+++ PictureArranger/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace PictureArranger
 {
@@ -20,9 +22,47 @@ namespace PictureArranger
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool isSourceOk = false;
+        bool isTargetOk = false;
         public MainWindow()
         {
+            
+        }
+
+        private void SourceButton_Click(object sender, RoutedEventArgs e)
+        {
             InitializeComponent();
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+            {
+                pathSourceTextBox.Text = dialog.FileName;
+                isSourceOk = true;
+            }
+        }
+
+        private void TargetButton_Click(object sender, RoutedEventArgs e)
+        {
+            InitializeComponent();
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+            {
+                pathSourceTextBox.Text = dialog.FileName;
+                isTargetOk = true;
+            }
+        }
+
+        private void copyButton_Click(object sender, RoutedEventArgs e)
+        {
+            DirectoryInfo dr = new DirectoryInfo(pathSourceTextBox.Text);
+            foreach (var file in dr.GetFiles())
+            {
+                
+            }
+
         }
     }
 }
